f0be03f3d0c83d51df8eead8a974cfe308c1fe3c
rk4226 <rk4226@nyu.edu>
2023-06-08 12:35:24 -0400

    added fourth led

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,6 +1,7 @@
 int ledPin = 13;
 int ledPin2 = 14;
 int ledPin3 = 15;
+int ledPin4 = 16;
 
 
 void setup() {
@@ -8,7 +9,7 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
-
+  pinMode(ledPin4, OUTPUT);
 }
 
 void loop() {
@@ -16,9 +17,11 @@ void loop() {
   digitalWrite(ledPin, HIGH);
   digitalWrite(ledPin2, HIGH);
   digitalWrite(ledPin3, HIGH);
+  digitalWrite(ledPin4, HIGH);
   delay(500);
   digitalWrite(ledPin, LOW);
   digitalWrite(ledPin2, LOW);
   digitalWrite(ledPin3, LOW);
+  digitalWrite(ledPin4, HIGH);
   delay(500);
 }
