653437015b6e3992e78251f576e5900ba4e6a4a0
rk4226 <rk4226@nyu.edu>
2023-06-08 12:32:44 -0400

    added third led

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,10 +1,12 @@
 int ledPin = 13;
 int ledPin2 = 14;
+int ledPin 3 = 15;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
 
 }
 
@@ -12,8 +14,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(ledPin, HIGH);
   digitalWrite(ledPin2, HIGH);
+  digitalWrite(ledPin3, HIGH);
   delay(500);
   digitalWrite(ledPin, LOW);
   digitalWrite(ledPin2, LOW);
+  digitalWrite(ledPin3, LOW);
   delay(500);
 }
